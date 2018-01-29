6c041fb4f812e37418b3ac8b8f313944024652b5
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2018-01-29 12:51:42 -0500

    fixed page number recognition

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 function scraper(){
 
-  let webPage = window.location.href.slice(0, window.location.href.length - 1); //url address of the page
-debugger
-  let lastPage = parseInt(Array.from(document.getElementsByClassName('a-pagination'))[0].children[6].children[0].text);
+  let webPage = getAdress(); //url address of the page ending with '='
   let i = 1;
+  window.location.href = webPage + i;
+  let lastPage = parseInt(Array.from(document.getElementsByClassName('a-pagination'))[0].children[6].children[0].text);
   let values = [];
   while(i <= lastPage){
-    window.location.href = window.location.href.slice(0, window.location.href.length - 2) + i;
+    window.location.href = webPage + i;
     let pageRevews = document.getElementsByClassName('a-section review');
-    debugger
     Array.from(pageRevews).forEach(review => {
       let stars = review.querySelector('.a-link-normal').title.slice(0, 3); //stars e.x. 5.0
       let title = review.querySelector('.review-title').innerText; // review title
       let author = review.querySelector('.a-row span a').innerText; // author name
       let email = '';
+      let body = review.querySelector('span.a-size-base.review-text').innerText; // body
       let date = review.querySelector('span.a-size-base.a-color-secondary.review-date').innerText.slice(3); //data
-      let reviewText = review.querySelector('span.a-size-base.review-text').innerText; // review
 
-      values.push([stars, title, author, email, date, reviewText]);
+      values.push([stars, title, author, email, body, date]);
 
     });
-    debugger
 
     i++;
   }
@@ -32,14 +30,24 @@ debugger
     csvContent += row + "\r\n";
   });
 
-var encodedUri = encodeURI(csvContent);
-var link = document.createElement("a");
-link.setAttribute("href", encodedUri);
-link.setAttribute("download", "my_data.csv");
-document.body.appendChild(link);
+  var encodedUri = encodeURI(csvContent);
+  var link = document.createElement("a");
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", "my_data.csv");
+  document.body.appendChild(link);
 
-link.click();
+  link.click();
 
 }
+
+const getAdress = () => {
+  let page = window.location.href;
+  for(let i = page.length - 1; i >= page.length - 4; i--){
+    if(page[i] === '='){
+      return page.slice(0, i + 1);
+    }
+  }
+  return page + '&pageNumber=';
+};
 
 scraper();
