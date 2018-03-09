e7be0dd3abe46c7aebf6db1ac755d34f586718ca
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2018-03-09 12:52:05 -0500

    added readme and description

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# —————— Work In Progress ——————
+# Browser console script for scraping reviews on amazon product
 
-## Basiс script to scrape all reviews to amazon products with immediate .csv export.
+
+## How it works:
+* Finds HTML object which contains all reviews on one pageReviews
+* Iterates through this elements children and extracts data like:
+  * Reviewer name
+  * Date of review (ex. ```November 18 2016```)
+  * Review stars (ex. ```5.0```)
+  * Review title
+  * Review text
+* Populates array object
+* Iterates through this object and adds joined data to future csv file(each array containing one review data on a line)
+* Encodes csv fili to URI
+* Creates a link to download file and hits this link
+* On output you get CSV file downloaded
+
+
+![Alt text](/assets/screen.png)
+
+
+### Want to try?
+  * Open index.js file and copy it's content
+  * Open chrome, select amazon product and open console(chrome dev tools)
+  * Paste script and hit Enter
+  
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function scraper(){
 
-  let values = [];
+  let values = []; // Object to store data
 
     let pageReviews = document.getElementById('cm-cr-dp-review-list'); // HTML object which contains all reviews
+
     Array.from(pageReviews.children).forEach((review) => {
       let stars = review.querySelector('.a-link-normal').title.slice(0, 3); //stars e.x. 5.0
       let title = review.querySelector('.review-title').innerText; // review title
@@ -16,23 +17,20 @@ function scraper(){
     });
 
   let csvContent = "data:text/csv;charset=utf-8,";
-  values.forEach(function(rowArray){
-    let row = rowArray.join(",");
-    csvContent += row + "\r\n";
+  values.forEach(function(rowArray){ // Iterating through array and addind lines to csv
+    let row = rowArray.join(","); // Joining one line of future csv with comma
+    csvContent += row + "\r\n"; // Adding data and new line
   });
 
-  let productTitle = document.getElementById('productTitle').innerText;
+  let productTitle = document.getElementById('productTitle').innerText; // Name of the product
 
 
-  var encodedUri = encodeURI(csvContent);
-  var link = document.createElement("a");
-  link.setAttribute("href", encodedUri);
-  link.setAttribute("download", `${productTitle}.csv`);
-  document.body.appendChild(link);
-
-  link.click();
-
+  var encodedUri = encodeURI(csvContent); // Encoding to URI
+  var link = document.createElement("a"); // Creating download link
+  link.setAttribute("href", encodedUri); // Adding attributes to link
+  link.setAttribute("download", `${productTitle}.csv`); // Download attribute and filename = product name
+  document.body.appendChild(link); // Adding link to the page
+  link.click(); // Hiting the link to download
 }
 
-
-scraper();
+scraper(); // to fire the script :-)
